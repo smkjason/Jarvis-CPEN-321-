37467abfb5ff67ed384966794f3c251722f160d8
Jason Song <smkjason.99@gmail.com>
2019-10-22 13:20:26 -0700

    Connection with server: in progress

--- Jarvis/app/src/main/java/com/example/jarvis/MainActivity.java
+++ Jarvis/app/src/main/java/com/example/jarvis/MainActivity.java
@@ -15,7 +15,30 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.tasks.Task;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.mortbay.util.IO;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -77,22 +100,10 @@ public class MainActivity extends AppCompatActivity {
 
         // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-//            try {
-//                GoogleSignInAccount account = task.getResult(ApiException.class);
-//                String authCode = account.getServerAuthCode();
-//
-//                // Show signed-un UI
-//                //updateUI(account);
-//                Intent intent = new Intent(MainActivity.this, LoggedIn.class);
-//                startActivity(intent);
-//                // TODO(developer): send code to server and exchange for access/refresh/ID tokens
-//            } catch (ApiException e) {
-//                Log.w("Error", "Sign-in failed", e);
-//                //updateUI(null);
-//            }
             // The Task returned from this call is always completed, no need to attach
             // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
             handleSignInResult(task);
         }
     }
@@ -100,9 +111,28 @@ public class MainActivity extends AppCompatActivity {
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            String authCode = account.getServerAuthCode(); //returns a one-time server auth code to send to your web server which can be exchanged for access token and sometimes refresh token if requestServerAuthCode(String) is configured; null otherwise. for details.
             // Signed in successfully, show authenticated UI.
             Intent intent = new Intent(MainActivity.this, LoggedIn.class);
             startActivity(intent);
+
+            /* Send the authentication code to backend server */
+            try{
+                URL url = new URL("http://ec2-3-14-144-180.us-east-2.compute.amazonaws.com");
+                HttpURLConnection URLconnection = (HttpURLConnection) url.openConnection();
+                URLconnection.setRequestMethod("POST");
+                URLconnection.setDoOutput(true);
+                URLconnection.setChunkedStreamingMode(0);
+
+                OutputStream output = URLconnection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(output, "UTF-8"));
+                writer.write(authCode);
+
+                URLconnection.disconnect();
+            }catch (IOException e){
+                Log.w("Error", "Connection Error");
+            }
+
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
