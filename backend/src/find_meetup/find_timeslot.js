3aad4be1dfae8fa316aab00b17c6f6aa07c52750
sunnyzhng <zhang.sunny_1999@hotmail.com>
2019-10-23 11:23:31 -0700

    fixed sum

--- backend/src/find_meetup/find_timeslot.js
+++ backend/src/find_meetup/find_timeslot.js
@@ -5,13 +5,12 @@ const moment = require('moment')
 
 const user_events = schema.EventModel
 
-var start_time
-var end_time 
+var start_time = {}
+var end_time = {}
 var start_to_end = []
 
-var s
-var e 
-var se 
+var start = 0;
+var set = {}
 
 var sum = [0,0,0,0,0,0,0,0,0,0,
 			0,0,0,0,0,0,0,0,0,0,
@@ -23,52 +22,53 @@ var sum = [0,0,0,0,0,0,0,0,0,0,
 
 function calculateBestTimeslot(listEvents){
 
+	var total = listEvents.size;
 	//incrementing through each user to get each user's start/end time
-	loop1:
-	for(var x = 0; x < listEvents.size; x++){
-		start_time = listEvents[x].start_time; 
-		end_time = listEvents[x].end_time;
+	// loop1:
+	// for(var x = 0; x < listEvents.size; x++){
+	// 	start_time = listEvents[x].start_time; 
+	// 	end_time = listEvents[x].end_time;
 
-		start_to_end = end_time.map(function(item, index){
-			return item - start_time[index];
-		})
+	// 	start_to_end = end_time.map(function(item, index){
+	// 		return item - start_time[index];
+	// 	})
 
-		loop2:
-		//increment the intervals of meet up time
-		for(var y = 0; y < start_to_end.length; y++){
-				while(start_to_end[y] > 0){
-					sum[start_time[y]] += 1;
-					start_to_end[y]--;
-					start_time[y]++;
-					if(start_to_end[y] < 0){
-						break loop2;
-					}
-				}
-			if(y = start_to_end.length){
-				break loop1;
-			}
-		}
-	 }
+	// 	loop2:
+	// 	//increment the intervals of meet up time
+	// 	for(var y = 0; y < start_to_end.length; y++){
+	// 			while(start_to_end[y] > 0){
+	// 				sum[start_time[y]] += 1;
+	// 				start_to_end[y]--;
+	// 				start_time[y]++;
+	// 				if(start_to_end[y] < 0){
+	// 					break loop2;
+	// 				}
+	// 			}
+	// 		if(y = start_to_end.length){
+	// 			break loop1;
+	// 		}
+	// 	}
+	//  }
+
+
 
 console.log(sum)
 
-var start = 0;
-var set = {}
 for(var i = 1; i < sum.length; i++){
-	if(sum[i] == 0){
+	if(sum[i] == total){
 		//starting a new sequence
-		if(sum[i-1] != 0){
+		if(sum[i-1] != total){
 			start = i;
 		}
 	} else {
 		//ending a sequence
-		if(sum[i-1] == 0){
+		if(sum[i-1] == total){
 			//take all the 0s from start - i-1
 			set[start] = i - 1
 		}
 	}
 }
-if(sum[sum.length-1] == 0){
+if(sum[sum.length-1] == total){
 	set[start] = sum.length - 1
 }
 
@@ -76,4 +76,8 @@ console.log(sum)
 console.log(set)
 
 return set;
+}
+
+module.exports = {
+	calculateBestTimeslot
 }
