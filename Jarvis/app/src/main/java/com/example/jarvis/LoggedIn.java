1d604edc4cf829cf09f6db635880ecbf49efd872
Jason Song <smkjason.99@gmail.com>
2019-10-22 18:50:42 -0700

    try download

--- Jarvis/app/src/main/java/com/example/jarvis/LoggedIn.java
+++ Jarvis/app/src/main/java/com/example/jarvis/LoggedIn.java
@@ -20,12 +20,24 @@ import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -35,6 +47,8 @@ public class LoggedIn extends AppCompatActivity {
     ImageView UserPhoto;
     TextView Username, Useremail/*, UserID*/;
     Button Signout;
+
+    Button Send;
 
     GoogleSignInClient mGoogleSignInClient;
 
@@ -57,6 +71,7 @@ public class LoggedIn extends AppCompatActivity {
         Username = findViewById(R.id.username);
         Useremail = findViewById(R.id.usergmail);
         Signout = findViewById(R.id.sign_out_button);
+        Send = findViewById(R.id.urlconnection);
 
         Signout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,26 +85,17 @@ public class LoggedIn extends AppCompatActivity {
                 }
             }
         });
-//
-//        public void SendMessage(View view){
-//
-//            try {
-//                URL url = new URL("http://ec2-3-14-144-180.us-east-2.compute.amazonaws.com");
-//                HttpURLConnection urlconnect = (HttpURLConnection) url.openConnection();
-//
-//                urlconnect.setRequestMethod("POST");
-//                urlconnect.setDoOutput(true);
-//                urlconnect.setChunkedStreamingMode(0);
-//
-//                OutputStream output = urlconnect.getOutputStream();
-//                BufferedWriter writer_2 = new BufferedWriter(new OutputStreamWriter(output, "UTF-8"));
-//                writer_2.write("Hello Can I write?? ");
-//            }catch (IOException e){
-//                Log.w("ERROR", "Cant Connect to the URL");
-//            }
-//
-//        }
 
+        Send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.urlconnection:
+                        sendMessage();
+                        break;
+                }
+            }
+        });
 
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
         if (acct != null) {
@@ -104,6 +110,42 @@ public class LoggedIn extends AppCompatActivity {
             Useremail.setText(personEmail);
             //UserID.setText(personId);
             Glide.with(this).load(String.valueOf(personPhoto)).into(UserPhoto);
+        }
+    }
+
+    private void sendMessage(){
+
+//        try {
+//            URL url = new URL("http://ec2-3-14-144-180.us-east-2.compute.amazonaws.com");
+//            HttpURLConnection urlconnect = (HttpURLConnection) url.openConnection();
+//
+//            urlconnect.setRequestMethod("POST");
+//            urlconnect.setDoOutput(true);
+//            urlconnect.setChunkedStreamingMode(0);
+//
+//            OutputStream output = urlconnect.getOutputStream();
+//            BufferedWriter writer_2 = new BufferedWriter(new OutputStreamWriter(output, "UTF-8"));
+//            writer_2.write("Hello Can I write??");
+//
+//        }catch (IOException e){
+//            Log.w("ERROR", "Cant Connect to the URL");
+//        }
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost("http://ec2-3-14-144-180.us-east-2.compute.amazonaws.com");
+
+        try {
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+            nameValuePairs.add(new BasicNameValuePair("Message", "Hello"));
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            HttpResponse response = httpClient.execute(httpPost);
+            int statusCode = response.getStatusLine().getStatusCode();
+            final String responseBody = EntityUtils.toString(response.getEntity());
+            Log.i("Error", "Signed in as: " + responseBody);
+        } catch (ClientProtocolException e) {
+            Log.e("Error", "Error sending ID token to backend.", e);
+        } catch (IOException e) {
+            Log.e("Error", "Error sending ID token to backend.", e);
         }
     }
 
--- Jarvis/app/src/main/java/com/example/jarvis/MainActivity.java
+++ Jarvis/app/src/main/java/com/example/jarvis/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jarvis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -16,6 +17,7 @@ import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 import org.apache.http.HttpResponse;
@@ -24,6 +26,7 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.mortbay.util.IO;
@@ -67,8 +70,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-//        String serverClientId = getString(R.string.server_client_id);
+        String serverClientId = getString(R.string.server_client_id);
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+               // .requestIdToken(serverClientId)
 //                .requestScopes(new Scope(Scopes.DRIVE_APPFOLDER))
 //                .requestServerAuthCode(serverClientId)
                 .requestEmail()
@@ -76,6 +80,16 @@ public class MainActivity extends AppCompatActivity {
 
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mGoogleSignInClient.silentSignIn()
+                .addOnCompleteListener(
+                        this,
+                        new OnCompleteListener<GoogleSignInAccount>() {
+                            @Override
+                            public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
+                                handleSignInResult(task);
+                            }
+                        });
     }
 
     /* Add this if we would like */
@@ -110,7 +124,27 @@ public class MainActivity extends AppCompatActivity {
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            //String authCode = account.getServerAuthCode(); //returns a one-time server auth code to send to your web server which can be exchanged for access token and sometimes refresh token if requestServerAuthCode(String) is configured; null otherwise. for details.
+//            String idToken = account.getIdToken();
+//            //String authCode = account.getServerAuthCode();
+//
+//            HttpClient httpClient = new DefaultHttpClient();
+//            HttpPost httpPost = new HttpPost("http://ec2-3-14-144-180.us-east-2.compute.amazonaws.com");
+//
+//            try {
+//                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+//                nameValuePairs.add(new BasicNameValuePair("idToken", idToken));
+//                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+//
+//                HttpResponse response = httpClient.execute(httpPost);
+//                int statusCode = response.getStatusLine().getStatusCode();
+//                final String responseBody = EntityUtils.toString(response.getEntity());
+//                Log.i("Error", "Signed in as: " + responseBody);
+//            } catch (ClientProtocolException e) {
+//                Log.e("Error", "Error sending ID token to backend.", e);
+//            } catch (IOException e) {
+//                Log.e("Error", "Error sending ID token to backend.", e);
+//            }
+             //returns a one-time server auth code to send to your web server which can be exchanged for access token and sometimes refresh token if requestServerAuthCode(String) is configured; null otherwise. for details.
             // Signed in successfully, show authenticated UI.
             Intent intent = new Intent(MainActivity.this, LoggedIn.class);
             startActivity(intent);
