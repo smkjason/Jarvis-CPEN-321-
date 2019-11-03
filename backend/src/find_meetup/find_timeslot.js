5bae49c680cd62239c01ae3e3448a6499ec458a8
sunnyzhng <zhang.sunny_1999@hotmail.com>
2019-11-02 18:39:56 -0700

    codacy fix on find_meetup

--- backend/src/find_meetup/find_timeslot.js
+++ backend/src/find_meetup/find_timeslot.js
@@ -1,79 +1,77 @@
-//const UserFunctions = require('./app/event')
-const mongoose = require('mongoose')
-const schema = require('../data/schema')
-const moment = require('moment')
+//const UserFunctions = require("./app/event");
+const mongoose = require("mongoose");
+const schema = require("../data/schema");
+const moment = require("moment");
 
-const user_events = schema.EventModel
+const user_events = schema.EventModel;
 
-var start_time = {}
-var end_time = {}
-var start_to_end = []
+var starttime = {};
+var endtime = {};
+var starttoend = [];
 
 var start = 0;
-var set = {}
+var set = {};
 
 var sum = [0,0,0,0,0,0,0,0,0,0,
 			0,0,0,0,0,0,0,0,0,0,
-			0,0,0,0]
+			0,0,0,0];
 
 
 //list of list of events
-//only need to grab start/end_time for each user
+//only need to grab start/end time for each user
 
 function calculateBestTimeslot(listEvents){
 
 	var total = listEvents.size;
-	//incrementing through each user to get each user's start/end time
-	// loop1:
-	// for(var x = 0; x < listEvents.size; x++){
-	// 	start_time = listEvents[x].start_time; 
-	// 	end_time = listEvents[x].end_time;
+	// incrementing through each user to get each user's start/end time
+	loop1:
+	for(var x = 0; x < listEvents.size; x++){
+		starttime = listEvents[x].start_time; 
+		endtime = listEvents[x].end_time;
 
-	// 	start_to_end = end_time.map(function(item, index){
-	// 		return item - start_time[index];
-	// 	})
+		starttoend = endtime.map(function(item, index){
+			return item - starttime[index];
+		})
 
-	// 	loop2:
-	// 	//increment the intervals of meet up time
-	// 	for(var y = 0; y < start_to_end.length; y++){
-	// 			while(start_to_end[y] > 0){
-	// 				sum[start_time[y]] += 1;
-	// 				start_to_end[y]--;
-	// 				start_time[y]++;
-	// 				if(start_to_end[y] < 0){
-	// 					break loop2;
-	// 				}
-	// 			}
-	// 		if(y = start_to_end.length){
-	// 			break loop1;
-	// 		}
-	// 	}
-	//  }
-
-
+		loop2:
+		//increment the intervals of meet up time
+		for(var y = 0; y < starttoend.length; y++){
+				while(starttoend[y] > 0){
+					sum[starttime[y]] += 1;
+					starttoend[y]--;
+					starttime[y]++;
+					if(starttoend[y] < 0){
+						break loop2;
+					}
+				}
+			if(y === starttoend.length){
+				break loop1;
+			}
+		}
+	 }
 
 console.log(sum)
 
 for(var i = 1; i < sum.length; i++){
-	if(sum[i] == total){
+	if(sum[i] === total){
 		//starting a new sequence
-		if(sum[i-1] != total){
+		if(sum[i-1] !== total){
 			start = i;
 		}
 	} else {
 		//ending a sequence
-		if(sum[i-1] == total){
+		if(sum[i-1] === total){
 			//take all the 0s from start - i-1
-			set[start] = i - 1
+			set[start] = i - 1;
 		}
 	}
 }
-if(sum[sum.length-1] == total){
-	set[start] = sum.length - 1
+if(sum[sum.length-1] === total){
+	set[start] = sum.length - 1;
 }
 
-console.log(sum)
-console.log(set)
+console.log(sum);
+console.log(set);
 
 return set;
 }
--- backend/src/find_meetup/temp.js
+++ backend/src/find_meetup/temp.js
@@ -1,54 +1,7 @@
-<<<<<<< HEAD
-var start_time = [[12,19],[13,20],[0,13]]
-var end_time = [[15,23],[16,23],[12,23]]
-var start_to_end = []
-
-var start = []
-var end = []
-var total = 2
-
-var sum = [0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0]
-
-loop1:
-for(var x = 0; x < 3; x++){
-	console.log(x)
-	start = start_time[x];
-	end = end_time[x];
-    console.log(start)
-    console.log(end)
-
-	start_to_end = end.map(function(item, index){
-		return item - start[index];
-    })
-    
-    console.log("start to end is" + start_to_end)
-	loop2:
-	//increment the intervals of meet up time
-	for(var y = 0; y < start_to_end.length; y++){
-			while(start_to_end[y] >=0){
-				sum[start[y]] += 1;
-				console.log(sum)
-				start_to_end[y]--;
-				start[y] += 1;
-				console.log("(INSIDE) start to end is" + start_to_end)
-				// if(start_to_end[y] < 0){
-				// 	break loop2;
-				// }
-			}
-		// if(y = start_to_end.length){
-		// 	break loop1;
-		// }
-	}
- }
-console.log(start_to_end)
-console.log(sum)
-=======
 const mongoose = require('mongoose')
 
 function init(){
-    var dbString = process.env.ENV == "production" ? 
+    var dbString = process.env.ENV === "production" ? 
         "mongodb+srv://jarvis:123123123@jarvis-kanro.mongodb.net/prod" : 
         "mongodb+srv://jarvis:123123123@jarvis-kanro.mongodb.net/test";
 
@@ -67,4 +20,3 @@ init();
 
 
 
->>>>>>> 537a80856f00e98d6f472a4cb0837c685b0d4dfc
