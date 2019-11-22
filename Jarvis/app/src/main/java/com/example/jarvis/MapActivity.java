43276395b0d25a6e92540b26572084ab91c210b3
Tony <tony.lee0754@gmail.com>
2019-11-22 10:15:54 -0800

    working on updating location

--- Jarvis/app/src/main/java/com/example/jarvis/MapActivity.java
+++ Jarvis/app/src/main/java/com/example/jarvis/MapActivity.java
@@ -2,9 +2,13 @@ package com.example.jarvis;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -14,15 +18,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
 
@@ -36,6 +57,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private Boolean mLocationPermissionsGranted = false;
     private GoogleMap mMap;
 
+    private String myemail;
+    private GoogleSignInAccount acct;
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
@@ -46,6 +70,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 //        mMap.setMaxZoomPreference(15f);
 
         if(mLocationPermissionsGranted) {
+            //mHandlerTask.run();
             getDeviceLocation();
 
             mMap.setMyLocationEnabled(true);
@@ -62,7 +87,11 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        getLocationPermission();
+        acct = GoogleSignIn.getLastSignedInAccount(this);
+        myemail = acct.getEmail();
+
+
+        createLocationRequest();
 
     }
 
@@ -82,6 +111,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                         if(task.isSuccessful() && task.getResult() != null) {
                             Log.d(TAG, "onComplete:found location!");
                             Location currentLocation = (Location) task.getResult();
+                            new updateLocation(Double.toString(currentLocation.getLatitude()),Double.toString(currentLocation.getLongitude())).execute();
 
                             moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),DEFAULT_ZOOM);
                         }
@@ -163,5 +193,116 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         }
     }
+
+    private final static int INTERVAL = 1000 * 5;//60 * 2; //2 minutes
+    Handler mHandler = new Handler();
+
+    Runnable mHandlerTask = new Runnable()
+    {
+        @Override
+        public void run() {
+            getDeviceLocation();
+            mHandler.postDelayed(mHandlerTask, INTERVAL);
+        }
+    };
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        mHandler.removeCallbacks(mHandlerTask); //stop updating location
+        finish();
+    }
+
+    private class updateLocation extends AsyncTask<Void, Void, Void> {
+
+        String lat;
+        String lon;
+
+        updateLocation(String lat, String lon) {
+            this.lat = lat;
+            this.lon = lon;
+        }
+
+        @Override
+        protected Void doInBackground(Void... v) {
+            try {
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpPut httpPut = new HttpPut("http://ec2-3-14-144-180.us-east-2.compute.amazonaws.com/user/" + acct.getEmail() + "/location");
+
+                JSONObject json = new JSONObject();
+                json.put("lat", lat);
+                json.put("lon", lon);
+
+                httpPut.setEntity(new StringEntity(json.toString()));
+                httpPut.setHeader("Content-Type", "application/json");
+
+                HttpResponse response = httpClient.execute(httpPut);
+                final String responseBody = EntityUtils.toString(response.getEntity());
+                Log.i("Information", "Location updated as " + responseBody);
+            } catch (Exception e) {
+                Log.e("Error", "I caught some exception.", e);
+            }
+            return null;
+        }
+
+
+    }
+
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+
+    protected void createLocationRequest() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                getLocationPermission();
+            }
+        });
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(MapActivity.this,
+                                REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        });
+    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        if (requestingLocationUpdates) {
+//            startLocationUpdates();
+//        }
+//    }
+//
+//    private void startLocationUpdates() {
+//        fusedLocationClient.requestLocationUpdates(locationRequest,
+//                locationCallback,
+//                Looper.getMainLooper());
+//    }
 
 }
