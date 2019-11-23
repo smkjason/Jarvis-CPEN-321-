10fe87bd16bf5d7d378705a5c6f8582276af689f
Tony <tony.lee0754@gmail.com>
2019-11-22 20:42:14 -0800

    go from chat to map works

--- Jarvis/app/src/main/java/com/example/jarvis/GroupChatActivity.java
+++ Jarvis/app/src/main/java/com/example/jarvis/GroupChatActivity.java
@@ -1,9 +1,12 @@
 package com.example.jarvis;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -258,5 +261,28 @@ public class GroupChatActivity extends AppCompatActivity {
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE);
 
         notificationManagerCompat.notify(msg_id, builder.build());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.chat_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.btnMap) {
+            Intent intent = new Intent(GroupChatActivity.this, MapActivity.class);
+            intent.putExtra("eventId", eventid);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
