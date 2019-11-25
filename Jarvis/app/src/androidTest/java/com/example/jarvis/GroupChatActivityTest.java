2bd4319f4d9c4ec5441d2bdbc3bd8b10bc198c9a
Jason Song <smkjason.99@gmail.com>
2019-11-25 13:08:40 -0800

    recording...

--- Jarvis/app/src/androidTest/java/com/example/jarvis/GroupChatActivityTest.java
+++ Jarvis/app/src/androidTest/java/com/example/jarvis/GroupChatActivityTest.java
@@ -1,17 +1,13 @@
 package com.example.jarvis;
 
 import android.content.Intent;
-import android.util.Log;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.mortbay.jetty.Main;
 
-import androidx.test.espresso.Espresso;
 import androidx.test.rule.ActivityTestRule;
-import io.socket.client.Socket;
 
 import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -31,20 +27,15 @@ public class GroupChatActivityTest {
     public final ActivityTestRule<GroupChatActivity> activityActivityTestRule =
             new ActivityTestRule<>(GroupChatActivity.class, true, false);
 
-    @Rule
-    public final ActivityTestRule<MainActivity> loginActivity =
-            new ActivityTestRule<>(MainActivity.class, true, true);
-
     @Before
     public void setUp() throws Exception {
-        onView(withId(R.id.sign_in_button))
-                .perform(click());
+
     }
 
     @Test
     public void sendmsgtest()
     {
-//        Intent intent = new Intent(Home.class, )
+
         activityActivityTestRule.launchActivity(null);
         onView(withId(R.id.edittext_chatbox))
                 .perform(typeText("Are you seeing this message?"));
